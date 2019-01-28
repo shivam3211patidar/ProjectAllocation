71d82ddbdf9605e586e34a8235d0fc3c64a2fbdd
Shivam Patidar <shivam.patidar@hashedin.com>
2019-01-28 19:54:21 +0530

    Date Format correction

--- src/ProjectAllocationTest.java
+++ src/ProjectAllocationTest.java
@@ -2,6 +2,7 @@ import org.junit.Test;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.util.Date;
 
 import static org.junit.Assert.assertEquals;
@@ -11,9 +12,8 @@ public class ProjectAllocationTest {
     @Test
     public void projectAllocationRequest() throws ParseException {
         SalesOrManagerImp salesOrManagerImp = new SalesOrManagerImp();
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-        String dateInString = sdf.format(new Date());
-        Date date = sdf.parse(dateInString);
+        LocalDate localDate = LocalDate.now();
+        Date date = java.sql.Date.valueOf(localDate);
         String projectDetails = "Curefit Lis is the module which gonna to be replaced with live health. The skill Set Required Spring Boot, Java8";
         int noOfPeopleRequired = 3;
         assertEquals("Completely",String.valueOf(salesOrManagerImp.projectAllocationRequest(date, projectDetails, noOfPeopleRequired)));
